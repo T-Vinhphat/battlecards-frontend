bb82d3b687bd778724beb12a0753f4ba2b8b96ea
vinhphat <vinhphat@live.fr>
2022-04-21 09:39:38 +0200

    create template cards

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="card">
+          <div>
+            <img
+              className="picture"
+              src="https://static.hitek.fr/img/actualite/ill_m/677768601/2438690.jpeg"
+            ></img>
+          </div>
+          <div className="power">
+            <h4>Power : 7</h4>
+          </div>
+        </div>
       </header>
     </div>
   );
