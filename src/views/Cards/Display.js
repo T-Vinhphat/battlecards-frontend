b092483137d6623e9bcfbf9d7408d64081ff3564
vinhphat <vinhphat@live.fr>
2022-04-25 09:10:45 +0200

    change card css templates

--- src/views/Cards/Display.js
+++ src/views/Cards/Display.js
@@ -2,19 +2,6 @@ import { Card } from "../../components/Card";
 import { useState, useEffect } from "react";
 import "./Display.css";
 
-const fakeList = [
-  {
-    imgSrc:
-      "https://static.hitek.fr/img/actualite/ill_m/677768601/2438690.jpeg",
-    cardPower: "5",
-  },
-  {
-    imgSrc:
-      "https://static.wikia.nocookie.net/heroes-fr/images/e/e3/King_OPM.webp/revision/latest/top-crop/width/360/height/450?cb=20210408080725&path-prefix=fr",
-    cardPower: "9",
-  },
-];
-
 function DisplayPage() {
   const [list, setList] = useState([]);
   console.log(list);
